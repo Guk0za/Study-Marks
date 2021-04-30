9a9c1880b91492c9999d6c5353c0dc4e0cf4fdf2
diademoff <diademoff@yandex.ru>
2021-04-30 07:41:45 +0300

    add statistic print

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
+import locale
+
+
+class LocalStrings:
+    language_index: int
+
+    def __init__(self, language_index: int):
+        self.language_index = language_index
+        self._line_read_error = [
+            "Ошибка во время чтения строки", "Error while reading line"]
+        self._current_marks = ["Текущие оценки", "Current marks"]
+        self._count = ["Количество", "Count of"]
+        self._to = ["до", "to"]
+
+    @property
+    def current_marks(self):
+        return self._current_marks[self.language_index]
+
+    @property
+    def line_read_error(self):
+        return self._line_read_error[self.language_index]
+
+    @property
+    def count(self):
+        return self._count[self.language_index]
+
+    @property
+    def to(self):
+        return self._to[self.language_index]
+
+
 class MarksList:
     """
     List of marks of study subject
@@ -13,7 +44,18 @@ class MarksList:
         """
         Returns the number of `mark_to_add` needed to get the `final_mark` equal `mark_aim` 
         """
-        temp_list = MarksList("", self.marks)
+        if self.final_mark < mark_aim:
+            # going to better mark
+            if mark_to_add < mark_aim:
+                # impossible
+                return 0
+        else:
+            # going to bad mark
+            if mark_to_add > mark_aim:
+                # impossible
+                return 0
+
+        temp_list = MarksList("", self.marks.copy())
         count = 0
         while True:
             if temp_list.final_mark == mark_aim:
@@ -55,3 +97,45 @@ class Parser:
             return MarksList(subjectTitle, marks)
         except:
             return None
+
+
+def get_lang():
+    if 'ru' in locale.getdefaultlocale()[0]:
+        return 0
+    else:
+        return 1
+
+
+def print_row_statistics(row: MarksList):
+    for mark_to_add in range(2, 6):
+        for mark_aim in range(2, 6):
+            marks_count_to_aim = row.predict(mark_to_add, mark_aim)
+            if marks_count_to_aim == 0:
+                # it is not corrent, so skip this
+                continue
+            print(
+                f'{local.count} \"{mark_to_add}\" {local.to} \"{mark_aim}\": {marks_count_to_aim}')
+
+
+parser = Parser()
+local = LocalStrings(get_lang())
+file_lines = open('marks.txt').readlines()
+marks_rows = []
+
+for line in file_lines:
+    line = line.strip()
+    if line == '':
+        continue
+    current_marks = parser.parseLine(line)
+    if current_marks == None:
+        print(f'{local.line_read_error}: \"{line}\"')
+        input()
+        exit(0)
+    marks_rows.append(current_marks)
+
+for row in marks_rows:
+    print(row.subjectTitle)
+    current_marks = ', '.join(list(map(lambda x: str(x), row.marks)))
+    print(f'{local.current_marks}: {current_marks}')
+    print_row_statistics(row)
+    print()
